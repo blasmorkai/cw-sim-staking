97bac886d4b8a569193e1d77cf1264cefb7b9400
Blas Morkai <blas.morkai@gmail.com>
2023-01-08 19:57:14 +0000

    small changes

--- src/state.rs
+++ src/state.rs
@@ -4,4 +4,4 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
-pub const NOTINUSE: Item<String> = Item::new("not_in_use_");
+pub const NOTINUSE: Item<String> = Item::new("not_in_use__");
